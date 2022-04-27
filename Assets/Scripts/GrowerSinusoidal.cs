efd4b6c24d2b9e5d8e53b275058c0916258383bb
Unknown <unknown@example.com>
2022-04-27 07:26:00 -0500

    growth code update

--- Assets/Scripts/GrowerSinusoidal.cs
+++ Assets/Scripts/GrowerSinusoidal.cs
@@ -4,79 +4,31 @@ using UnityEngine;
 
 public class GrowerSinusoidal : MonoBehaviour
 {
+    float theta = 0f;
+    float amplitude = 2f;
 
-    private float scaleSpeed = 5;
-
-    private bool getSmaller = false;
+    private float scaleSpeed;
+    private float scaleIncrement = .1f;
 
     [SerializeField] Vector3 scaleDirection = new Vector3(1, 1, 1);
 
     [SerializeField] private Vector3 maxSize = new Vector3(2, 2, 2);
     [SerializeField] private Vector3 minSize = new Vector3(1, 1, 1);
+    [SerializeField] private Vector3 nowSize;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        nowSize = transform.localScale;
     }
-
     // Update is called once per frame
     void Update()
     {
-        //sucks to suck
-        IsGettingSmaller();
-        IsGettingBigger();
 
+        theta += scaleIncrement;
+        scaleSpeed = Mathf.Sin(theta) * amplitude;
+
+        transform.localScale = nowSize + (scaleDirection * scaleSpeed * Time.deltaTime);
+        Debug.Log("scale speed is " + scaleSpeed);
     }
 
-    void IsGettingBigger()
-    {
-        if (getSmaller == false)
-        {
-            if (transform.localScale.x < maxSize.x)
-            {
-                transform.localScale += (scaleDirection * scaleSpeed * Time.deltaTime);
-            }
-            if (transform.localScale.x > maxSize.x)
-            {
-                getSmaller = true;
-            }
-        }
-    }
-
-    void IsGettingSmaller()
-    {
-        if (getSmaller == true)
-        {
-            if (transform.localScale.x < minSize.x)
-            {
-                getSmaller = false;
-            }
-            if (transform.localScale.x > minSize.x)
-            {
-                transform.localScale -= (scaleDirection * scaleSpeed * Time.deltaTime);
-            }
-        }
-    }
 }
-
-/*
- float theta = 0.0;
-float amplitude = 75.0;
-float yvalues;
-
-void setup() {
-  size(640, 360);
-}
-
-void draw() {
-  background(0);
-
-  theta += 0.02;
-    yvalues = cos(theta)*amplitude;
-    
-      noStroke();
-  fill(255);
-    ellipse(10, height/2+yvalues, 16, 16);
-}
- */
