97e92171e82d2bcab34a6123db7388b7054d353f
hontema <leeclement455@gmail.com>
2022-04-30 01:56:10 -0500

    spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] Transform[] spawnPoint;
+    [SerializeField] float timeUntilSpawnedObjectDestroy = 15f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,23 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spawnDestroy();
+        }
+    }
+
+    void spawnDestroy()
+    {
+
+        //randomindex = Random.Range(0, 3);
+        for (int i = 0; i < spawnPoint.Length; i++)
+        {
+
+
+            GameObject newSpawn = Instantiate(objectToSpawn, spawnPoint[i].position, spawnPoint[i].rotation);
+            //Destroy(newSpawn, timeuntilspawnedobjectdestroy);
+            Debug.Log(spawnPoint[i].position +"sep"+ spawnPoint[i].rotation);
+        }
     }
 }
